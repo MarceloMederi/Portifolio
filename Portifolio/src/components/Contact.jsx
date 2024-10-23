fb55976238ed78310c2f8fb3913bad7f6af08bbc
Marcelo Medeiros dos Santos <marcelomederi11@gmail.com>
2024-10-23 14:48:54 -0300

    Colocado ao icone Envelope GitHub e Linkedin, nas tag A da pagina Contact

--- Portifolio/src/components/Contact.jsx
+++ Portifolio/src/components/Contact.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './styles/Contact.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Contact = () => {
     return (
         <section id="Contact">
-
             <h2>Contato</h2>
             
             <div className="contact">
@@ -12,26 +14,24 @@ const Contact = () => {
 
                 <ul className="contact-list">
                     <li>
-                        Email: 
+                        <FontAwesomeIcon icon={faEnvelope} /> Email: 
                         <a href="mailto:marcelomederi11@gmail.com" target="_blank" rel="noopener noreferrer"> marcelomederi11@gmail.com</a>
                     </li>
 
                     <li>
-                        GitHub: 
+                        <FontAwesomeIcon icon={faGithub} /> GitHub: 
                         <a href="https://github.com/MarceloMederi" target="_blank" rel="noopener noreferrer"> GitHub</a>
                     </li>
 
                     <li>
-                        LinkedIn: 
+                        <FontAwesomeIcon icon={faLinkedin} /> LinkedIn: 
                         <a href="https://www.linkedin.com/in/marcelo-medeiros-dos-santos-9aa50b1a7" target="_blank" rel="noopener noreferrer"> LinkedIn</a>
                     </li>
-
                 </ul>  
-
             </div>
-            
         </section>
     );
 };
+
 
 export default Contact;
