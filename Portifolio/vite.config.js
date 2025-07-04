845d5b814d3f4af8c8628ed89f190b7e2372bc03
Marcelo Medeiros Santos <marcelomederi11@gmail.com>
2025-07-04 17:38:01 -0300

    Alterado o vite.config.js para colocar o caminho da base do projeto

--- Portifolio/vite.config.js
+++ Portifolio/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // ou o caminho base do seu projeto, se necessário
   plugins: [react()],
 })
