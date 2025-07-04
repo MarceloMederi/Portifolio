44a996ffa432d7e11bde6f3cc4c075a59cd0a634
Marcelo Medeiros Santos <marcelomederi11@gmail.com>
2025-07-04 17:14:48 -0300

    Alterado o nome da pasta certificados, para Certificados. As imagens dos Certificados não estava de acordo com a importação. E fiz a alteração da tag <p> na pagina Introduction.jsx para deixar mais legivel

--- Portifolio/src/components/Introduction.jsx
+++ Portifolio/src/components/Introduction.jsx
@@ -9,10 +9,15 @@ const Introduction = () => {
             <h2>Sobre Mim</h2> {/* Título da seção */}
 
             <p>
-            Marcelo Medeiros dos Santos {/* Nome do profissional */}<br />
-            Sou um profissional de TI entusiasta, com uma forte paixão por tecnologia e desenvolvimento de software. Possuo experiência sólida em suporte técnico e análise de dados, o que me permite entender profundamente as necessidades dos usuários e traduzir essas demandas em soluções eficazes.<br />
-            Atualmente, atuo como Analista de Testes na NTT DATA EUROPE & LATAM, onde tenho a oportunidade de aprimorar minhas habilidades em testes manuais e na elaboração de cenários, sempre respeitando rigorosamente os parâmetros estabelecidos. Além disso, estou profundamente envolvido no desenvolvimento de projetos em React, onde aplico minhas competências técnicas para criar interfaces dinâmicas e responsivas. Embora a telemetria seja um interesse secundário, continuo explorando suas aplicações.<br />
-            Recentemente, direcionei meu foco para a inteligência artificial, buscando automatizar processos e explorar áreas com grande potencial, como entrevistas, monitoramento de comportamento, agilidade em processos e análise de dados.<br /> Estou sempre em busca de novas tecnologias e melhores práticas, com o objetivo de contribuir de forma significativa em ambientes desafiadores e inovadores.
+
+                <br />Sou um profissional de TI entusiasta, com uma forte paixão por tecnologia e desenvolvimento de software. 
+                Com uma experiência sólida em suporte técnico e análise de dados, consigo entender profundamente as necessidades dos usuários e traduzir essas demandas em soluções eficazes.<br />
+
+                <br />Atualmente, atuo como Analista de Testes na NTT DATA EUROPE & LATAM, onde aprimoro minhas habilidades em testes manuais e na elaboração de cenários, sempre respeitando rigorosamente os parâmetros estabelecidos. <br />
+
+                <br />Estou também profundamente envolvido no desenvolvimento de projetos envolventes, aplicando minhas competências técnicas para criar interfaces dinâmicas e responsivas.
+                Embora a telemetria seja um interesse secundário, continuo explorando suas aplicações.<br />
+
             </p>
 
         </section>
