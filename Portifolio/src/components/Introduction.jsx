3dcafe4aae7d9c5a4a260a23bf32da5c2adba0bc
Marcelo Medeiros Santos <marcelomederi11@gmail.com>
2025-07-07 11:23:26 -0300

    Colocado responsividade na Introduction

--- Portifolio/src/components/Introduction.jsx
+++ Portifolio/src/components/Introduction.jsx
@@ -8,16 +8,16 @@ const Introduction = () => {
 
             <h2>Sobre Mim</h2> {/* Título da seção */}
 
-            <p>
-
-                <br />Sou um profissional de TI entusiasta, com uma forte paixão por tecnologia e desenvolvimento de software. 
-                Com uma experiência sólida em suporte técnico e análise de dados, consigo entender profundamente as necessidades dos usuários e traduzir essas demandas em soluções eficazes.<br />
-
-                <br />Atualmente, atuo como Analista de Testes na NTT DATA EUROPE & LATAM, onde aprimoro minhas habilidades em testes manuais e na elaboração de cenários, sempre respeitando rigorosamente os parâmetros estabelecidos. <br />
-
-                <br />Estou também profundamente envolvido no desenvolvimento de projetos envolventes, aplicando minhas competências técnicas para criar interfaces dinâmicas e responsivas.
-                Embora a telemetria seja um interesse secundário, continuo explorando suas aplicações.<br />
-
+            <p className="introduction-text">
+                Sou um profissional de TI entusiasta, com uma forte paixão por tecnologia e desenvolvimento de software. 
+                Com uma experiência sólida em suporte técnico e análise de dados, consigo entender profundamente as necessidades dos usuários e traduzir essas demandas em soluções eficazes.
+            </p>
+            <p className="introduction-text">
+                Atualmente, atuo como Analista de Testes na NTT DATA EUROPE & LATAM, onde aprimoro minhas habilidades em testes manuais e na elaboração de cenários, sempre respeitando rigorosamente os parâmetros estabelecidos.
+            </p>
+            <p className="introduction-text">
+                Estou também profundamente envolvido no desenvolvimento de projetos envolventes, aplicando minhas competências técnicas para criar interfaces dinâmicas e responsivas.
+                Embora a telemetria seja um interesse secundário, continuo explorando suas aplicações.
             </p>
 
         </section>
